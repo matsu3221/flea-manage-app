2eb306c07e7149c9862b90058a07923fd79b2cef
matsu3221 <yu0i0so0n@gmail.com>
2023-11-18 22:07:01 +0900

    ナビゲーションバー、各種コンポーネント追加、ルーティング追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonNavbarComponent } from './common/common-navbar/common-navbar.component';
+import { ProductManageComponent } from './product-manage/product-manage.component';
+import { ListingManageComponent } from './listing-manage/listing-manage.component';
+import { TotalSalesManegeComponent } from './total-sales-manege/total-sales-manege.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommonNavbarComponent,
+    ProductManageComponent,
+    ListingManageComponent,
+    TotalSalesManegeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/total-sales-manege/total-sales-manege.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-total-sales-manege',
+  templateUrl: './total-sales-manege.component.html',
+  styleUrls: ['./total-sales-manege.component.scss']
+})
+export class TotalSalesManegeComponent {
+
+}
